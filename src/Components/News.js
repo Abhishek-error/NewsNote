1fc2cf11d79680fd884721334b799eb906ec5780
Mr. <elvishbaba282+github@gmail.com>
2024-06-03 18:19:40 +0530

    fixing some issues

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,60 +1,59 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import NewsItem from './NewsItem';
-import PropTypes from 'prop-types';
-import Spinner from './Spinner';
-import InfiniteScroll from 'react-infinite-scroll-component';
+import React, { useState, useEffect } from 'react'
+import NewsItem from './NewsItem'
+import propTypes from 'prop-types'
+import Spinner from './Spinner'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 const News = (props) => {
-  const [page, setPage] = useState(1);
-  const [totalResults, setTotalResults] = useState(0);
-  const [loading, setLoading] = useState(true);
-  const [articles, setArticles] = useState([]);
+  const [page, setPage] = useState(1)
+  const [totalResults, setTotalResults] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const [articles, setArticles] = useState([])
 
   const capitalizeFirstLetter = (str) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
-  };
+  }
 
   const fetchMoreData = async () => {
-    const newPage = page + 1;
-    setPage(newPage);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&page=${newPage}&pageSize=${props.pageSize}`;
-
+    setPage(prevPage => prevPage + 1)
     try {
+      let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=cf5022c9f3394adf9bdeccdaadbcf95a&page=${page + 1}&pageSize=${props.pageSize}`;
       let data = await fetch(url);
       let parsedData = await data.json();
-      setArticles((prevArticles) => prevArticles.concat(parsedData.articles));
-      setTotalResults(parsedData.totalResults);
+      setArticles(prevArticles => prevArticles.concat(parsedData.articles))
+      setTotalResults(parsedData.totalResults)
     } catch (error) {
       console.error("Error fetching more news:", error);
     }
-  };
+  }
 
-  const updateNews = useCallback(async () => {
+  const updateNews = async () => {
     props.setProgress(10);
-    setLoading(true);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${process.env.REACT_APP_NEWS_API_KEY}&page=1&pageSize=${props.pageSize}`;
-    props.setProgress(30);
+    setLoading(true)
     try {
+      let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=cf5022c9f3394adf9bdeccdaadbcf95a&page=${page}&pageSize=${props.pageSize}`;
+      props.setProgress(30);
       let data = await fetch(url);
       props.setProgress(50);
       let parsedData = await data.json();
       props.setProgress(80);
       setArticles(parsedData.articles);
-      setTotalResults(parsedData.totalResults);
+      setTotalResults(parsedData.totalResults)
       setLoading(false);
       props.setProgress(100);
-      setPage(1); // Reset page to 1
     } catch (error) {
       console.error("Error fetching news:", error);
       setLoading(false);
-      props.setProgress(100);
     }
-  }, [props]);
+  }
 
   useEffect(() => {
     updateNews();
+
     document.title = `${capitalizeFirstLetter(props.category)} - NewsSpot`;
-  }, [props.category, props.country, props.pageSize, updateNews]);
+    // Reset page number when category or country changes
+    setPage(1);
+  }, [props.category, props.country, props.pageSize]); // Ensure that dependencies are fixed
 
   return (
     <>
@@ -73,7 +72,7 @@ const News = (props) => {
                 <div className="col-md-4" key={element.url}>
                   <NewsItem
                     imgUrl={element.urlToImage}
-                    discription={element.description ? element.description.slice(0, 60) : ""}
+                    description={element.description ? element.description.slice(0, 60) : ""}
                     title={element.title ? element.title.slice(0, 35) : ""}
                     newsUrl={element.url}
                     source={element.source.name}
@@ -81,26 +80,26 @@ const News = (props) => {
                     author={element.author}
                   />
                 </div>
-              );
+              )
             })}
           </div>
         </div>
       </InfiniteScroll>
     </>
-  );
-};
+  )
+}
 
 News.defaultProps = {
   country: 'in',
   pageSize: 6,
-  category: 'general',
-};
+  category: 'general'
+}
 
 News.propTypes = {
-  country: PropTypes.string,
-  pageSize: PropTypes.number,
-  category: PropTypes.string,
-  setProgress: PropTypes.func.isRequired,
-};
+  country: propTypes.string,
+  pageSize: propTypes.number,
+  category: propTypes.string,
+  setProgress: propTypes.func.isRequired
+}
 
-export default News;
+export default News
